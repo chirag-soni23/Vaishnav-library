300737482d9580c51ba2b4b49638b7fe5c872cea
Chirag Soni <csoni0693@gmail.com>
2025-02-27 20:09:29 +0530

    library

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Images from '../components/Images'
 import Attendance from '../components/Attendance'
+import Card from '../components/Card'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <Images/>
       <Attendance/>
+      <Card/>
     </div>
   )
 }
