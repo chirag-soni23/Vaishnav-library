e8acbe731d50bccb1bbccccbf21431164671992b
Chirag Soni <csoni0693@gmail.com>
2025-02-27 10:10:45 +0530

    library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/register'element={<Register/>}/>
+        <Route path='/login'element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Book, Eye, EyeOff, Lock, Mail, Phone, User } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="min-h-screen ">
+      <div className="flex justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* LOGO */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
+              >
+                <Book className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">Get started with your free account</p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type={showPassword ? "password" : "text"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="••••••••"
+                
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button  type="submit" className="btn btn-primary w-full">
+             Login
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Not a member?{" "}
+              <Link to="/register" className="link link-primary">
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
