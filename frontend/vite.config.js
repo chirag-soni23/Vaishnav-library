c13320469303e947af62b1176f82999790d5713a
Chirag Soni <csoni0693@gmail.com>
2025-02-28 17:40:53 +0530

    library

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000/"
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000/"
       }
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000 // Set the chunk size warning limit to 2MB
   }
 })
