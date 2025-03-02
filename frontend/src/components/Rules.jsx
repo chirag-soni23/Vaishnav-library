7ef32f4a4868781a94c04ffa89234edba025e96f
Chirag Soni <csoni0693@gmail.com>
2025-03-02 15:06:37 +0530

    library

--- frontend/src/components/Rules.jsx
+++ frontend/src/components/Rules.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { rulesList } from "../constants/Rules.js"; 
+import { rulesList } from "../constants/Rules.js";
 
 const Rules = () => {
   return (
     <>
-    <div className="overflow-x-auto p-4">
-    <h1 className="text-3xl font-bold text-center mb-5 mt-10">Rules</h1>
-      <table className="table mt-10 w-full border-collapse border border-gray-300">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border p-2 text-black">#</th>
-            <th className="border p-2 text-black">Library Rules</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rulesList.map((rule, index) => (
-            <tr key={index} className="border">
-              <td className="border p-2 text-center">{index + 1}</td>
-              <td className="border p-2">{rule}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <div className="p-4">
+        <h1 className="text-3xl font-bold text-center mb-5 mt-10">Rules</h1>
+        {rulesList.map((rule, index) => (
+          <div key={index} className="mb-2">
+            <div tabIndex={0} className="collapse collapse-plus bg-base-100 border-base-300 border">
+              <div className="collapse-title font-semibold">Rule #{index + 1}</div>
+              <div className="collapse-content text-sm">
+                {rule}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
