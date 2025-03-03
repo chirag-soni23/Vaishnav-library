ecd3cd5c508d26fb311efee37514cd534ce1bba9
Chirag Soni <csoni0693@gmail.com>
2025-03-03 20:41:36 +0530

    library

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ app.use("/api/contact", contactRoutes);
 //   res.send('Hello Chirag, Your Backend is Working!');
 // });
 
+
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/dist")))
 app.get("*", (req, res) => {
