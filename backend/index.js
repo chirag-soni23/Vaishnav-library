da62683a9c85b03eebf2060c22ac68d0463b3dd9
Chirag Soni <csoni0693@gmail.com>
2025-05-25 17:20:56 +0530

    path

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import userRoutes from './routes/userRoutes.js';
 import attendanceRoutes from './routes/attendanceRoutes.js';
 import contactRoutes from './routes/contactRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
-// import path from "path";
+import path from "path";
 import cors from "cors";
 import compression from 'compression';
 import cloudinary from 'cloudinary';
@@ -35,15 +35,15 @@ app.use('/api/attendance', attendanceRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/ai", aiRoutes);
 
-app.get("/",(req, res) => {
-  res.send('Server is live!');
-});
+// app.get("/",(req, res) => {
+//   res.send('Server is live!');
+// });
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "/frontend/dist")))
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-// })
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
